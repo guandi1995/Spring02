6b366124967d1b6a692e602f8ff5768dd1babfb0
Di Guan <dguan@purdue.edu>
2021-07-27 21:02:46 +0800

    upload for test

---
+++ src/main/java/com/test.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author Di Guan on 7/27/2021 9:00 PM
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test for springMVC");
+    }
+}
